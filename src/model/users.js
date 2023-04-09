7d5a2fcb0721405c281ae1be960d65373e545a21
renan.moraes <renan.moraes@localiza.com>
2023-04-09 08:22:35 -0300

    create a new query that checks is an user exists

--- src/model/users.js
+++ src/model/users.js
@@ -1,14 +1,24 @@
 const db = require('../database/connection.js');
 
-const createUser = async (user) => {
+const createUser = async (body) => {
   const createUser = ({
     text: 'INSERT INTO users (name, password, username) VALUES ($1, $2, $3) RETURNING id',
-    values: [user.name, user.password, user.username]
+    values: [body.name, body.password, body.username]
   });
 
-  return db.query(createUser)
+  return db.query(createUser);
+}
+
+const checkUserExistsByUsername = async (username) => {
+  const checkUserExistsByUsername = ({
+    text: 'SELECT EXISTS(SELECT 1 FROM USERS WHERE username = $1)',
+    values: [username]
+  });
+
+  return db.query(checkUserExistsByUsername);
 }
 
 module.exports = {
-  createUser
+  createUser,
+  checkUserExistsByUsername
 };
